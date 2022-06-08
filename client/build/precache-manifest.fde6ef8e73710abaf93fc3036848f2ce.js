7e25432a67d6fcba44126eeccbad76655c436ec4
chi_tej <18ucs088@lnmiit.ac.in>
2022-06-08 14:17:40 +0530

    first commit

---
+++ client/build/precache-manifest.fde6ef8e73710abaf93fc3036848f2ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9186c2ab98be448b83c7abcbb4133f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea1830caed8f744d67f9",
+    "url": "/static/css/main.e55ada25.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1830caed8f744d67f9",
+    "url": "/static/js/main.f7055380.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/WeShare.5c0c63fb.png"
+  }
+]);
