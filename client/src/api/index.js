584656694f4933255a07e5d89b40267ff1a44e37
chi_tej <18ucs088@lnmiit.ac.in>
2022-06-08 14:20:46 +0530

    second commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const url = 'https://wesharee.herokuapp.com/posts';
-
+const subject = 'we share app';
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
